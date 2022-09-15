0fc9c081db891da914c9b5432f74f71165f4f260
Matthias Kolley <matthias.kolley@googlemail.com>
2022-09-15 10:43:30 +0200

    Refactoring -> Remove unused code

--- core/src/main/java/com/github/naxos84/ai/AiTile.java
+++ core/src/main/java/com/github/naxos84/ai/AiTile.java
@@ -9,38 +9,17 @@ public class AiTile extends Coord {
     int gridX;
     int gridY;
 
-    int index;
-    boolean selected;
-
     public AiTile(float x, float y, int gridX, int gridY) {
         super(Math.round(x), Math.round(y));
         this.gridX = gridX;
         this.gridY = gridY;
     }
 
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public boolean isSelected() {
-        return selected;
-    }
-
-    public void toggleSelection() {
-        this.selected = !this.selected;
-    }
-
     public void render(ShapeRenderer shapeRenderer, boolean inPath) {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        if (inPath && !selected) {
+        if (inPath) {
             // green
             shapeRenderer.setColor(.57f, .76f, .48f, 1);
-        } else if (selected) {
-            shapeRenderer.setColor(0.57f, 0f, 0f, 1f);
         } else {
             // blue
             shapeRenderer.setColor(.8f, .88f, .95f, 1);
